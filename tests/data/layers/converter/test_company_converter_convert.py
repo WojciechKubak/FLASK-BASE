8096adc3e7c592b021a899fdccd5ca36ba3401d4
WojciechKubak <wojciechkubak99@gmail.com>
2023-06-25 21:34:21 +0200

    Fixing type hinting in Converter layer tests

--- tests/data/layers/converter/test_company_converter_convert.py
+++ tests/data/layers/converter/test_company_converter_convert.py
@@ -1,10 +1,9 @@
 from app.data.layers.converter import CompanyConverter
 from app.data.organization.company import Company
-from typing import Any
 
 
 class TestCompanyConverterConvert:
 
-    def test_if_converter_returns_expected_class_obj(self, company_record_test: dict[str, Any]) -> None:
+    def test_if_converter_returns_expected_class_obj(self, company_record_test):
         result = CompanyConverter().convert(company_record_test)
         assert isinstance(result, Company)
