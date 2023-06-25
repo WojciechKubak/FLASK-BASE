68001b5d29bf24e958036c170c036f70231a1aa5
WojciechKubak <wojciechkubak99@gmail.com>
2023-06-25 21:39:22 +0200

    Fixing type hinting in Validator layer tests

--- tests/data/layers/validator/test_company_json_validator_validate.py
+++ tests/data/layers/validator/test_company_json_validator_validate.py
@@ -1,23 +1,13 @@
-from app.data.layers.validator import CompanyJsonValidator
-from typing import Any
 import pytest
 
 
 class TestCompanyJsonValidatorValidate:
 
-    def test_when_company_data_is_not_correct(
-            self,
-            company_record_test: dict[str, Any],
-            company_validator_obj: CompanyJsonValidator
-    ) -> None:
+    def test_when_company_data_is_not_correct(self, company_record_test, company_validator_obj):
         company_record_test['city'] = 'Incorrect1City'
         with pytest.raises(ValueError) as err:
             company_validator_obj.validate(company_record_test)
         assert 'city: does not match condition' == str(err.value)
 
-    def test_when_company_data_is_correct(
-            self,
-            company_record_test: dict[str, Any],
-            company_validator_obj: CompanyJsonValidator
-    ) -> None:
+    def test_when_company_data_is_correct(self, company_record_test, company_validator_obj):
         assert company_validator_obj.validate(company_record_test) == company_record_test
--- tests/data/layers/validator/test_validator_validate_value.py
+++ tests/data/layers/validator/test_validator_validate_value.py
@@ -1,18 +1,16 @@
 from app.data.layers.validator import Validator
-from typing import Any
-import pytest
 
 
 class TestValidatorValidateValue:
 
-    def test_when_key_not_found_in_data(self, company_record_test: dict[str, Any]) -> None:
+    def test_when_key_not_found_in_data(self, company_record_test):
         assert Validator.validate_value(company_record_test, 'non_existing_key', lambda x: x) == 'key not found'
 
-    def test_when_value_does_not_match_condition(self, company_record_test: dict[str, Any]) -> None:
+    def test_when_value_does_not_match_condition(self, company_record_test):
         assert Validator.validate_value(company_record_test, 'company_name', lambda x: False) \
                == 'does not match condition'
 
-    def test_when_value_matches_condition(self, company_record_test: dict[str, Any]) -> None:
+    def test_when_value_matches_condition(self, company_record_test):
         assert not Validator.validate_value(company_record_test, 'company_name', lambda x: True)
 
 
