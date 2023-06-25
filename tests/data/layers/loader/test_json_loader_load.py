06f29f75fb8488675e19f06ad199f66c4591969d
WojciechKubak <wojciechkubak99@gmail.com>
2023-06-25 21:22:02 +0200

    Setting path as Loader class attribute

--- tests/data/layers/loader/test_json_loader_load.py
+++ tests/data/layers/loader/test_json_loader_load.py
@@ -4,25 +4,24 @@ import pytest
 
 class TestJsonLoaderLoadFailure:
 
-    def test_when_path_has_incorrect_extension(self, company_test_path: str) -> None:
+    def test_when_path_has_incorrect_extension(self, company_test_path):
         with pytest.raises(AttributeError) as err:
-            JsonLoader().load(f'{company_test_path}ff')
+            JsonLoader(f'{company_test_path}nn').load()
         assert 'Incorrect file extension.' == str(err.value)
 
     def test_when_path_does_not_exist(self) -> None:
         with pytest.raises(FileNotFoundError) as err:
-            JsonLoader().load('tests/example_data/company_dataa.json')
+            JsonLoader('tests/example_data/company_dataa.json').load()
         assert str(err.value).startswith('File not found')
 
 
 class TestJsonLoaderLoaderSuccess:
 
-    def test_when_loaded_json_is_empty(self, company_empty_test_path: str) -> None:
-        result = JsonLoader().load(company_empty_test_path)
+    def test_when_loaded_json_is_empty(self, company_empty_test_path):
+        result = JsonLoader(company_empty_test_path).load()
         assert 0 == len(result)
 
-    def test_when_loaded_json_contains_data(self, company_test_path: str) -> None:
-        result = JsonLoader().load(company_test_path)
+    def test_when_loaded_json_contains_data(self, company_test_path):
+        result = JsonLoader(company_test_path).load()
         assert 2 == len(result)
-        # todo: czy coś tu jeszcze dodać w ramach walidacji json'a?
         assert all(len(record) == 7 for record in result)
