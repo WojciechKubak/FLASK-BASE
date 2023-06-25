43a1bcc8614fa6a401b7d3040b01f7869b1839ae
WojciechKubak <wojciechkubak99@gmail.com>
2023-06-25 21:22:20 +0200

    Setting path as Loader class attribute

--- app/data/layers/loader.py
+++ app/data/layers/loader.py
@@ -6,20 +6,21 @@ import json
 
 @dataclass
 class Loader(ABC):
+    path: str
 
     @abstractmethod
-    def load(self, path: str) -> list[dict[str, Any]]:
+    def load(self) -> list[dict[str, Any]]:
         pass
 
 
 @dataclass
 class JsonLoader(Loader):
 
-    def load(self, path: str) -> list[dict[str, Any]]:
-        if not path.endswith('.json'):
+    def load(self) -> list[dict[str, Any]]:
+        if not self.path.endswith('.json'):
             raise AttributeError('Incorrect file extension.')
         try:
-            with open(path, 'r', encoding='utf-8') as json_data:
+            with open(self.path, 'r', encoding='utf-8') as json_data:
                 return json.load(json_data, parse_int=str, parse_float=str)
         except Exception as e:
             raise FileNotFoundError(f'File not found: {e}.')
