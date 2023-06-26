69dc46c4569aca930d8436f520c54a30c2bf35ba
WojciechKubak <wojciechkubak99@gmail.com>
2023-06-26 19:33:08 +0200

    Removing type hints from organization layer tests

--- tests/data/organization/employee/test_employee_from_dict.py
+++ tests/data/organization/employee/test_employee_from_dict.py
@@ -7,11 +7,11 @@ import pytest
 class TestEmployeeFromDict:
 
     @pytest.mark.skip('This is not prepared to be tested.')
-    def test_when_data_is_not_correct(self) -> None:
+    def test_when_data_is_not_correct(self):
         with pytest.raises(AttributeError) as err:
             Employee.from_dict({})
         assert str(err.value).startswith('Data is not correct.')
 
-    def test_when_data_is_correct(self, employee_record_test: dict[str, Any]) -> None:
+    def test_when_data_is_correct(self, employee_record_test):
         employee = Employee.from_dict(employee_record_test)
         assert employee.salary == Decimal('7000')
