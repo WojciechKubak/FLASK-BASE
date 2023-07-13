eaf25869f05f6d3a550805c6e7ad14916d26a049
Wojciech Kubak <wojciechkubak99@gmail.com>
2023-07-13 17:37:30 +0200

    Testing Employee.get_performace_average method

--- tests/data/organization/employee/test_employee_get_performance_average.py
+++ tests/data/organization/employee/test_employee_get_performance_average.py
@@ -1,17 +1,12 @@
-from typing import Any
-from unittest.mock import patch, PropertyMock
 from app.data.models.employee import Employee
-import pytest
+from typing import Any
 
 
 def test_employee_get_performance_average_works_correctly(employee_record_test: dict[str, Any]) -> None:
-    # todo: (1)
-    """
-    1. jaki powinen przyjąć standard co do pola performance_rating w klasie Employee, żeby później nie było z tym problemu?
-    2. jak miałyby wyglądać testy metody employee.get_performance_average()? (frozenInstance)
-    """
-    # employee_obj = Employee.from_dict(employee_record_test)
-    # with patch.object(employee_obj, 'performance_rating', new_callable=PropertyMock) as attr_mock:
-    #     attr_mock.return_value = {'x': 1, 'y': 2, 'z': 3}
-    #     assert 2.0 == employee_obj.get_performance_average()
-    ...
+    employee_record_test['performance_rating'] = {
+         "efficiency": 4,
+         "creativity": 5,
+         "communication": 3
+    }
+    employee = Employee.from_dict(employee_record_test)
+    assert 4. == employee.get_performance_average()
