3d2bc0835d4d65eafe929b56f50efcbfe33e7bec
WojciechKubak <wojciechkubak99@gmail.com>
2023-07-20 18:17:35 +0200

    Refactoring Company model tests.

--- tests/data/model/company/test_company_from_dict.py
+++ tests/data/model/company/test_company_from_dict.py
@@ -1,7 +1,24 @@
 from app.data.model.company import Company
+from app.data.model.employee import Employee
 from typing import Any
 
 
-def test_when_data_is_correct(company_record_test: dict[str, Any]) -> None:
-    result = Company.from_dict(company_record_test)
-    assert isinstance(result, Company)
+class TestCompanyFromDict:
+
+    def test_when_data_has_no_employee(self, company_record_data: dict[str, Any]) -> None:
+        result = Company.from_dict(company_record_data)
+        assert isinstance(result.id_, int)
+        assert not result.employees
+
+    def test_when_data_has_employee_ids(self, company_record_data: dict[str, Any]) -> None:
+        company_record_data['employees'] = [1, 2, 3]
+        result = Company.from_dict(company_record_data)
+        assert 3 == len(result.employees)
+
+    def test_when_data_has_employee_objects(
+            self,
+            company_record_data: dict[str, Any],
+            employee_obj_mock: Employee) -> None:
+        company_record_data['employees'] = [employee_obj_mock, employee_obj_mock]
+        result = Company.from_dict(company_record_data)
+        assert 2 == len(result.employees)
