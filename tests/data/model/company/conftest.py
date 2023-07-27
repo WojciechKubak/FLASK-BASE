451a2de4cfe6f1e2b30e1ed3ae5d689b93dedb45
WojciechKubak <wojciechkubak99@gmail.com>
2023-07-27 18:43:13 +0200

    Fixing type hint.

--- tests/data/model/company/conftest.py
+++ tests/data/model/company/conftest.py
@@ -5,7 +5,7 @@ import pytest
 
 
 @pytest.fixture
-def employee_obj_mock() -> Employee | MagicMock:
+def employee_obj_mock() -> MagicMock:
     employee_mock = MagicMock(spec=Employee)
     employee_mock.to_dict.return_value = {}
     employee_mock.__str__.return_value = 'employee data'
