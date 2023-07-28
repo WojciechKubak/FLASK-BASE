b422c2cb1e156525b496a950315b701245ff3bfe
WojciechKubak <wojciechkubak99@gmail.com>
2023-07-28 11:33:21 +0200

    Refactoring Employee.to_dict tests.

--- tests/data/model/employee/test_employee_to_dict.py
+++ tests/data/model/employee/test_employee_to_dict.py
@@ -6,5 +6,5 @@ def test_employee_to_dict_conversion(employee_record_data: dict[str, Any]) -> No
     employee = Employee.from_dict(employee_record_data)
     result = employee.to_dict()
     assert len(employee_record_data) == len(result)
-    assert 2 == result['company']
+    assert int(employee_record_data['company']) == result['company']
     assert 'id' in result
