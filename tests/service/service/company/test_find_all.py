6e92c0dc5267dcc15b99287f6fd622efb147372b
WojciechKubak <wojciechkubak99@gmail.com>
2023-07-28 11:00:47 +0200

    Fixing spacing.

--- tests/service/service/company/test_find_all.py
+++ tests/service/service/company/test_find_all.py
@@ -4,4 +4,3 @@ from app.service.service.company import CompanyService
 def test_find_all(company_service: CompanyService) -> None:
     result = company_service.find_all()
     assert company_service.company_repository.companies == result
-
