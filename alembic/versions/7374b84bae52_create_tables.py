667ec717c5c58aa418a718ded6cd65f884ecb88b
WojciechKubak <wojciechkubak99@gmail.com>
2023-08-06 14:45:35 +0200

    Adding record creation date to table models

--- alembic/versions/7374b84bae52_create_tables.py
+++ alembic/versions/7374b84bae52_create_tables.py
@@ -9,6 +9,9 @@ from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy.sql import func
+
+from datetime import datetime
 
 
 # revision identifiers, used by Alembic.
@@ -25,15 +28,15 @@ def upgrade() -> None:
         sa.Column('company_name', sa.String(255)),
         sa.Column('street', sa.String(255)),
         sa.Column('city', sa.String(255)),
-        sa.Column('postal_code', sa.String(255)),
-        sa.Column('state', sa.String(255)),
+        sa.Column('postal_code', sa.String(20)),
+        sa.Column('state', sa.String(20)),
         sa.Column('country', sa.String(255)),
+        sa.Column('created_at', sa.TIMESTAMP, default=func.now(), server_default=sa.text('CURRENT_TIMESTAMP')),
     )
     op.create_table(
         'employees',
         sa.Column('id', sa.Integer, primary_key=True, autoincrement=True),
-        sa.Column('first_name', sa.String(255)),
-        sa.Column('last_name', sa.String(255)),
+        sa.Column('full_name', sa.String(255)),
         sa.Column('position', sa.String(255)),
         sa.Column('age', sa.Integer),
         sa.Column('employment_tenure', sa.String(255)),
@@ -42,6 +45,7 @@ def upgrade() -> None:
         sa.Column('performance_rating', sa.JSON),
         sa.Column('company_id', sa.Integer),
         sa.ForeignKeyConstraint(['company_id'], ['companies.id']),
+        sa.Column('created_at', sa.TIMESTAMP, default=func.now(), server_default=sa.text('CURRENT_TIMESTAMP')),
     )
 
 
