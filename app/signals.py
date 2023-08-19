e57ff8d8fb2be884e4e12067fda42d86f3bf2eea
WojciechKubak <wojciechkubak99@gmail.com>
2023-08-19 15:05:04 +0200

    Adding additional signals info

--- app/signals.py
+++ app/signals.py
@@ -1,4 +1,4 @@
-from flask import Flask, request_started
+from flask import Flask, request_started, request
 from typing import Any
 import logging
 
@@ -9,6 +9,9 @@ def log_request_started(sender: Flask, **extras: dict[str, Any]) -> None:
     logging.info('Request started')
     logging.info(f'{sender=}')
     logging.info(f'{extras=}')
+    logging.info(f"URL: {request.url}")
+    logging.info(f"Method: {request.method}")
+    logging.info(f"Headers: {request.headers}")
 
 
 request_started.connect(log_request_started)
